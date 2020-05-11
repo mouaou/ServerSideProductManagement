0b6e7c61351610e829ca05ba8046d58b7ce83ce1
YMOUAOU <youssef.dev0101@gmail.com>
2020-05-11 13:18:50 +0200

    first commit

---
+++ src/main/java/com/example/demo/ServerSideProductManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSideProductManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSideProductManagementApplication.class, args);
+	}
+
+}
